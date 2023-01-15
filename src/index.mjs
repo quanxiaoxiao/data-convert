bfb08f8b282e1dab225fdd28ad407a39f1229611
quan <quan_xiaoxiao@qq.com>
2023-01-15 18:51:18 +0800

    xxx

--- src/index.mjs
+++ src/index.mjs
@@ -1,25 +1,8 @@
 import convertDataValue from './convertDataValue.mjs';
 import checkDataValid from './checkDataValid.mjs';
+import convertData from './convertData.mjs';
 
-const convert = (schema) => {
-  const keys = Object.keys(schema);
-  return (data) => keys.reduce((acc, key) => {
-    const dataType = schema[key];
-    if (typeof dataType === 'function') {
-      return {
-        ...acc,
-        [key]: dataType(data[key]),
-      };
-    }
-    const v = convertDataValue(data[key], dataType);
-    return {
-      ...acc,
-      [key]: v,
-    };
-  }, {});
-};
-
-export default convert;
+export default convertData;
 
 export {
   convertDataValue,
